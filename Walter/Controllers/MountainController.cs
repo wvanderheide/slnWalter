b6b62a51f378a59719eb9133e53b6a1feff2cdcf
walter.vanderheide <walter.vanderheide@klasresearch.com>
2015-09-21 16:40:07 -0600

    Form Validation

--- Walter/Controllers/MountainController.cs
+++ Walter/Controllers/MountainController.cs
@@ -9,90 +9,89 @@ using Mountain = Walter.Models.Mountain;
 
 namespace Walter.Controllers
 {
-    public class MountainController : Controller
-    {
-        public ActionResult AddLog()
-        {
-            if (string.IsNullOrEmpty(Request.QueryString["MountainID"]))
-            {
-                return RedirectToAction("Index");
-            }
+	public class MountainController : Controller
+	{
+		public ActionResult AddLog()
+		{
+			if (string.IsNullOrEmpty(Request.QueryString["MountainID"]))
+			{
+				return RedirectToAction("Index");
+			}
 
-            ViewBag.MountainName = Request.QueryString["MountainName"];
-            ViewBag.MountainID = Request.QueryString["MountainID"];
-            return View();
-        }
+			ViewBag.MountainName = Request.QueryString["MountainName"];
+			ViewBag.MountainID = Request.QueryString["MountainID"];
+			return View();
+		}
 
-        public ActionResult AddMountain()
-        {
-            return View("AddMountain", new ViewModels.Mountain());
-        }
+		public ActionResult AddMountain()
+		{
+			return View("AddMountain", new ViewModels.Mountain());
+		}
 
-        public ActionResult SaveMountain(ViewModels.Mountain m, string summitDate, string summitNote)
-        {
-	        if (ModelState.IsValid && !string.IsNullOrWhiteSpace(summitDate))
-	        {
-		        var x = new MountainBusinessLayer();
-		        x.SaveMountain(m, summitDate, summitNote);
-	        }
-	        else
-	        {
+		public ActionResult SaveMountain(ViewModels.Mountain m, string summitDate, string summitNote)
+		{
+			DateTime tempDate;
+			if (!ModelState.IsValid || !DateTime.TryParse(summitDate, out tempDate))
 				return View("AddMountain", m);
-	        }
-
-	        return RedirectToAction("Index");
-        }
 
 
-        public ActionResult SaveLog(int mountainId, string summitDate, string summitNote)
-        {
-            var x = new MountainBusinessLayer();
-            x.SaveLog(mountainId, summitDate, summitNote);
+			var biz = new MountainBusinessLayer();
+			if (!biz.SaveMountain(m, summitDate, summitNote))
+				return View("AddMountain", m);
 
-            return RedirectToAction("Index");
-        }
+			return RedirectToAction("Index");
+		}
 
-        //[ChildActionOnly]
-        //public ActionResult GetAddNewLink()
-        //{
-        //    if (Convert.ToBoolean(Session["IsAdmin"]))
-        //    {
-        //        return PartialView("_AddNewLink");
-        //    }
-        //    else
-        //    {
-        //        return new EmptyResult();
-        //    }
-        //}
 
-        public ActionResult Index()
-        {
-            //TODO:  Once I add authentication this Session Var will be come "real";
-            Session["IsAdmin"] = true;
+		public ActionResult SaveLog(int mountainId, string summitDate, string summitNote)
+		{
+			var x = new MountainBusinessLayer();
+			x.SaveLog(mountainId, summitDate, summitNote);
 
-	        var b = new MountainBusinessLayer();
-            var mtns = b.GetMountains();
+			return RedirectToAction("Index");
+		}
 
-	        var mountainList = mtns.Select(mtn => new ViewModels.Mountain
-	        {
-		        Id = mtn.id, 
-				Name = mtn.Name, 
-				Elevation = mtn.Elevation, 
-				Country = mtn.Country, 
-				State = mtn.State, 
-				Latitude = mtn.Latitude, 
-				Longitude = mtn.Longitude, 
-				MountainNote = mtn.MountainNote, 
+		//[ChildActionOnly]
+		//public ActionResult GetAddNewLink()
+		//{
+		//    if (Convert.ToBoolean(Session["IsAdmin"]))
+		//    {
+		//        return PartialView("_AddNewLink");
+		//    }
+		//    else
+		//    {
+		//        return new EmptyResult();
+		//    }
+		//}
+
+		public ActionResult Index()
+		{
+			//TODO:  Once I add authentication this Session Var will be come "real";
+			Session["IsAdmin"] = true;
+
+			var b = new MountainBusinessLayer();
+			var mtns = b.GetMountains();
+
+			var mountainList = mtns.Select(mtn => new ViewModels.Mountain
+			{
+				Id = mtn.id,
+				Name = mtn.Name,
+				Elevation = mtn.Elevation,
+				Country = mtn.Country,
+				State = mtn.State,
+				Latitude = mtn.Latitude,
+				Longitude = mtn.Longitude,
+				MountainNote = mtn.MountainNote,
 				SummitLog = mtn.MountainSummitLogs.Select(l => new ViewModels.MountainSummitLog
-		        {
-			        Id = l.id, 
-					MountainId = l.MountainID, 
-					SummitDate = Convert.ToDateTime(l.SummitDate), 
+				{
+					Id = l.id,
+					MountainId = l.MountainID,
+					SummitDate = Convert.ToDateTime(l.SummitDate),
 					SummitNote = l.SummitNote
-		        }).ToList().OrderBy(y => y.SummitDate).ToList()
-	        }).ToList();
-			
-	        return View("Index", new MountainViewModel { Mountains = mountainList });
-        }
-    }
+				}).ToList().OrderBy(y => y.SummitDate).ToList()
+			}).ToList();
+
+			return View("Index", new MountainViewModel { Mountains = mountainList.OrderByDescending(x => x.SummitLog.Last().SummitDate).ToList() });
+		}
+	}
 }
