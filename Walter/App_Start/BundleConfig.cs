68b5671eaae9f11f196ca762c17daefc58dcaa8e
wvanderheide <walter.vanderheide@klasresearch.com>
2015-09-21 22:02:03 -0600

    Jquery not working....

--- Walter/App_Start/BundleConfig.cs
+++ Walter/App_Start/BundleConfig.cs
@@ -19,13 +19,13 @@ namespace Walter
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            //          "~/Scripts/bootstrap.js",
-            //          "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
 
             // Include line below to bundle Javascript and minimize them.  
             //BundleTable.EnableOptimizations = true;
