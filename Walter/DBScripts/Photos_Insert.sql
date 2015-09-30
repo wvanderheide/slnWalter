d8c731cb1263043fd75b08b3646ab95f18db4043
wvanderheide <walter.vanderheide@klasresearch.com>
2015-09-29 20:25:58 -0600

    Photos

--- Walter/DBScripts/Photos_Insert.sql
+++ Walter/DBScripts/Photos_Insert.sql
@@ -6,8 +6,8 @@ INSERT INTO [dbo].[Photos]
            ,[URL]
            ,[Date])
      VALUES
-           ('Yosemite'
-           ,'https://picasaweb.google.com/107082436175597733376/Yosemite?authuser=0&authkey=Gv1sRgCN26pqDe1pPNYA&feat=directlink'
-           ,'Oct 8, 2014')
+           ('Timp'
+           ,'https://picasaweb.google.com/107082436175597733376/TimpWithKLAS?authuser=0&authkey=Gv1sRgCIGamdOdlOyBYw&feat=directlink'
+           ,'9/25/2015')
 GO
 
