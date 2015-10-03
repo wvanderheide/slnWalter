2dc647a4b516b8353d4fe8de4da11edffcb86c23
wvanderheide <walter.vanderheide@klasresearch.com>
2015-10-02 21:32:56 -0600

    Photos through 2012

--- Walter/DBScripts/Photos_Insert.sql
+++ Walter/DBScripts/Photos_Insert.sql
@@ -6,8 +6,8 @@ INSERT INTO [dbo].[Photos]
            ,[URL]
            ,[Date])
      VALUES
-           ('Maple Ice'
-           ,'https://picasaweb.google.com/107082436175597733376/MapleCanyon?authuser=0&authkey=Gv1sRgCInU5NrRhbfEogE&feat=directlink'
-           ,'1/19/2013')
+           ('Alberta'
+           ,''
+           ,'12/25/2012')
 GO
 
