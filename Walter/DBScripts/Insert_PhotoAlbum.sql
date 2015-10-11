dcec39edc37c169c0fcd7361358c5c27104855a4
wvanderheide <walter.vanderheide@klasresearch.com>
2015-10-11 14:49:01 -0600

    331 albums, 291 climbs, 159 mtns

---
+++ Walter/DBScripts/Insert_PhotoAlbum.sql
@@ -0,0 +1,14 @@
+USE [Walter]
+GO
+
+INSERT INTO [dbo].[Photos]
+           ([Title]
+           ,[URL]
+           ,[Date])
+     VALUES
+           ('Over the Years'
+           ,'https://vanman798.shutterfly.com/pictures/2218'
+           ,'1/1/1972')
+GO
+
+
