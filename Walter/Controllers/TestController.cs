e3babcdd972786078ff221cb7f0675e6194aa5db
wvanderheide <walter.vanderheide@klasresearch.com>
2015-10-17 11:07:44 -0600

    Pre -Playing with Partial View

---
+++ Walter/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Walter.Models;
+
+namespace Walter.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            var b = new TestBusinessLayer();
+            return View("Index", b.GetAllProducts(4));
+        }
+    }
+}
---
+++ Walter/Models/TestBusinessLayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Walter.ViewModels;
+
+namespace Walter.Models
+{
+    public class TestBusinessLayer
+    {
+        public List<VMphoto> GetAllProducts(int takeThisMany)
+        {
+            var db = new WalterEntities();
+            List<VMphoto> prods = db.Photos.ToList().Select(x => new VMphoto
+            {
+                Title = x.Title,
+                Id = x.id
+            }).OrderBy(y => y.Title).Take(takeThisMany).ToList();
+
+            return prods;
+        }
+    }
+}
