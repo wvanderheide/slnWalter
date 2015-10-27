d6ca5a378ed332104c84e5ce65481b9e9dbbb7be
walter.vanderheide <walter.vanderheide@klasresearch.com>
2015-10-27 14:29:50 -0600

    Playing with bundling vs CDN - ended up leaving things as is, just renamed bundles to js, and content to CSS

--- Walter/App_Start/BundleConfig.cs
+++ Walter/App_Start/BundleConfig.cs
@@ -8,28 +8,31 @@ namespace Walter
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new ScriptBundle("~/js/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            //bundles.Add(new ScriptBundle("~/js/jqueryval").Include(
+              //          "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            bundles.Add(new ScriptBundle("~/js/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new ScriptBundle("~/js/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/js/site").Include(
+                       "~/Scripts/site.js"));
 
             //bundles.Add(new StyleBundle("~/Content/css").Include(
             //          "~/Content/bootstrap.css",
             //          "~/Content/site.css"));
 
-             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+             bundles.Add(new StyleBundle("~/css/site").Include("~/Content/site.css"));
             
-            // Include line below to bundle Javascript and minimize them.  
+            // Include line below to bundle Javascript and minify them.  
             BundleTable.EnableOptimizations = true;
         }
     }
