e703d00f58f007c0906969e7204dfc79c392a1c2
walter.vanderheide <walter.vanderheide@klasresearch.com>
2015-11-13 09:38:17 -0700

    Cancel Elmah logging

--- Walter/Global.asax.cs
+++ Walter/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Elmah;
 
 namespace Walter
 {
@@ -17,5 +19,25 @@ namespace Walter
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        protected void ErrorLog_Filtering(object sender, ExceptionFilterEventArgs e)
+        {
+            // Cancels elmah logging for these conditions.
+
+            //if (!Request.Url.ToString().ToLower().Contains(ConfigurationManager.AppSettings["klasresearchURL"], true) || Request.UserAgent.ToLower().Contains("bot", true) || Request.UserAgent.ToLower().Contains("spider", true) || Request.UserAgent.ToLower().Contains("crawler", true) || Request.UserAgent.ToLower().Contains("slurp", true) || Request.UserAgent.ToLower().Contains("ezooms", true))
+            //{
+
+            //    e.Dismiss();
+
+            //}
+            
+            //if (Request.Url.ToString().ToLower().Contains("jw-icons.eot"))
+            //{
+
+            //    e.Dismiss();
+
+            //}
+
+        }
+
     }
 }
